c674a43567420b7296815ed1584dda1175ed10e0
vitorbsdc <vitor.bsdc@gmail.com>
2022-06-01 17:07:31 -0300

    Modified page studies

--- src/pages/Studies/index.js
+++ src/pages/Studies/index.js
@@ -15,171 +15,157 @@ const Studies = () => {
   return (
     <>
       <NavBar />
+      <div class="boxStudies p-3">
+        <h1 className="font-monospace fs-4">
+          Abaixo são algumas das principais "Hard Skills" adquiridas por meio do
+          curso "Técnico em Desenvolvimento de Sistemas", na Escola Senai
+          Suíço-Brasileira Paulo Ernesto Tolle.
+        </h1>
+      </div>
+
       <div class="container-sm">
         <div class="row row-cols-2 row-cols-lg-3 g-2 g-lg-3">
           <div className="col">
             <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Javascript}
-                    class="img-fluid rounded-start"
-                    alt="study-2"
-                  />
+              <div class="col-md-4">
+                <img
+                  src={Javascript}
+                  class="img-fluid rounded-start"
+                  alt="study-2"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">
+                    Javascript
+                  </h5>
                 </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">
-                      Javascript
-                    </h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Html}
-                    class="img-fluid rounded-start"
-                    alt="study-2"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">HTML5</h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Css}
-                    class="img-fluid rounded-start"
-                    alt="study-3"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">CSS3</h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Git}
-                    class="img-fluid rounded-start"
-                    alt="study-4"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">GIT</h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Reactjs}
-                    class="img-fluid rounded-start"
-                    alt="study-5"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">React</h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Bootstrap}
-                    class="img-fluid rounded-start"
-                    alt="study-6"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">
-                      Bootstrap
-                    </h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Mysql}
-                    class="img-fluid rounded-start"
-                    alt="study-7"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">
-                      My SQL
-                    </h5>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Mongodb}
-                    class="img-fluid rounded-start"
-                    alt="study-8"
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">
-                      Mongo DB
-                    </h5>
-                  </div>
               </div>
             </div>
           </div>
           <div className="col">
             <div class="card mb-3">
-                <div class="col-md-4">
-                  <img
-                    src={Frontend}
-                    class="img-fluid rounded-start"
-                    alt="study-8"
-                  />
+              <div class="col-md-4">
+                <img src={Html} class="img-fluid rounded-start" alt="study-2" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">HTML5</h5>
                 </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title text-center font-monospace">
-                      Front end
-                    </h5>
-                  </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img src={Css} class="img-fluid rounded-start" alt="study-3" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">CSS3</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img src={Git} class="img-fluid rounded-start" alt="study-4" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">GIT</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img
+                  src={Reactjs}
+                  class="img-fluid rounded-start"
+                  alt="study-5"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">React</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img
+                  src={Bootstrap}
+                  class="img-fluid rounded-start"
+                  alt="study-6"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">
+                    Bootstrap
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img
+                  src={Mysql}
+                  class="img-fluid rounded-start"
+                  alt="study-7"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">My SQL</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img
+                  src={Mongodb}
+                  class="img-fluid rounded-start"
+                  alt="study-8"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">
+                    Mongo DB
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card mb-3">
+              <div class="col-md-4">
+                <img
+                  src={Frontend}
+                  class="img-fluid rounded-start"
+                  alt="study-8"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title text-center font-monospace">
+                    Front end
+                  </h5>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="boxStudies">
-        <h1 className="font-monospace fs-4 ">
-          Acima são algumas das principais "Hard Skills" adquiridas por meio do
-          curso "Técnico em Desenvolvimento de Sistemas", na Escola Senai
-          Suíço-Brasileira Paulo Ernesto Tolle.
-        </h1>
       </div>
     </>
   );
